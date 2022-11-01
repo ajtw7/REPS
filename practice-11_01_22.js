f72c77b1af8674237be6907d3be68417ce955ab2
Andrew <ayjtw7@gmail.com>
2022-11-01 14:44:08 -0400

    solidyfying differences between map and forEach

---
+++ practice-11_01_22.js
@@ -0,0 +1,24 @@
+let firstNames = ["Jerome", "Ritchie", "Clyde", "Michael", "Darnell", "Steph"];
+console.log(firstNames);
+
+let printFirstNames = firstNames.map((name) => {
+  console.log(`My first name is ${name}.`);
+});
+
+printFirstNames;
+
+const uppercaseFirstName = firstNames[0].toUpperCase();
+console.log(uppercaseFirstName);
+
+const uppercaseFN = firstNames.map((name) => {
+  return `My first name is ${name.toUpperCase()}!`;
+});
+
+console.log(uppercaseFN);
+
+const uppercaseFN2 = firstNames.forEach((name) => {
+  console.log();
+});
+
+
+console.log(uppercaseFN2)
